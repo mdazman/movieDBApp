bfb7e1722e7e32ab08a9d6d4fdfe975245ba2c76
AZMAN MUHAMMAD <mdazman@MacBook-Pro.lan>
2022-06-14 12:46:12 +0800

    first commit

---
+++ MovieDBApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieDBApp
+//
+//  Created by AZMAN MUHAMMAD on 14/6/22.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieDBApp/MovieDBAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  MovieDBAppApp.swift
+//  MovieDBApp
+//
+//  Created by AZMAN MUHAMMAD on 14/6/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieDBAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
